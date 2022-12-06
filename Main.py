4bf76872b10a8c4dc389fc6829dc78d4f51fe137
15755 <zf990318@bu.edu>
2022-12-05 20:16:58 -0500

    Completed the implementation of image recongnition application with pretrained vgg16 model

--- Main.py
+++ Main.py
@@ -5,19 +5,15 @@ import requests
 from PIL import Image
 import torch.nn.functional as F
 from torchvision import models, transforms
-
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask import request, Flask, render_template
 import time
 
 
-# labels for image net classification
-LABELS_URL = 'https://s3.amazonaws.com/mlpipes/pytorch-quick-start/labels.json'
-labels = {int(key):value for (key, value)
-          in requests.get(LABELS_URL).json().items()}
+result = ""
 
-# Data preprocess
+#Data pre-process
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -26,12 +22,16 @@ preprocess = transforms.Compose([
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225])
 ])
+# Gett labels for image classification
+LABELS_URL = 'https://s3.amazonaws.com/mlpipes/pytorch-quick-start/labels.json'
+labels = {int(key):value for (key, value)
+          in requests.get(LABELS_URL).json().items()}
 
-# ResNet initialization
-model = models.resnet18(pretrained=True).eval()
+
+# Set vgg16 pretrained model
+model = models.vgg16(pretrained=True).eval()
 
 
-# web servers
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
@@ -40,11 +40,12 @@ def index():
 
 
 @app.route('/', methods=['GET', 'POST'])
-def upload():
+def predict():
     if request.method == 'POST':
-        # Receive the file
+        # Record start time
         start_time = time.time()
-        f = request.files['image']
+        #recived image file
+        file = request.files['image']
 
         # Save the file
         root = os.path.dirname(__file__)
@@ -52,19 +53,21 @@ def upload():
         if not os.path.exists(img_folder):
             os.makedirs(img_folder)
 
-        img_path = os.path.join(img_folder, secure_filename(f.filename))
-        f.save(img_path)
+        img_path = os.path.join(img_folder, secure_filename(file.filename))
+        file.save(img_path)
 
         # Recognize the image
-        img_pil = Image.open(img_path)
+        img_pil = Image.open(img_path).convert("RGB")
         img_tensor = preprocess(img_pil)
         img_tensor.unsqueeze_(0)
         fc_out = model(img_tensor)
         fc_out = F.softmax(fc_out)
         lbl_out = np.argsort(fc_out[0].data.numpy())[-1]
-        result = " Top-1 prediction:\"" + str(labels[lbl_out]) + " \";  Confidence: %.3f" % (fc_out[0].data.numpy()[lbl_out]) + ", Response Time: %.3f s" % (time.time() - start_time)
+        end_time = time.time()
+        result = " Predict Result: " + str(labels[lbl_out]) + ",  Confidence: " + str((fc_out[0].data.numpy()[lbl_out])) + ",  Response Time: " + str((end_time - start_time))
         return result
-    return None
+    else:
+        return None
 
 
 if __name__ == '__main__':
--- static/functions.js
+++ static/functions.js
@@ -1,34 +1,35 @@
+
+//functions that read image file and trigger button
 $(document).ready(function () {
-    // Init
     $('.image-section');
     $('#result');
 
-    // Upload Preview
-    function readURL(input) {
+    // Upload image and display preview of it
+    function readFile(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
+                $('#imageView').css('background-image', 'url(' + e.target.result + ')');
+                $('#imageView').fadeIn(300);
             }
             reader.readAsDataURL(input.files[0]);
         }
     }
-    $("#imageUpload").change(function () {
+
+    //Show image preview and submit button after upload image
+    $("#upload").change(function () {
         $('.image-section').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
-        readURL(this);
+        $('#submit-button').show();
+        readFile(this);
     });
 
-    $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
+    //Display predict result of submit image
+    $('#submit-button').click(function () {
+        var image_input = new FormData($('#image-file')[0]);
 
         $.ajax({
             type: 'POST',
-            data: form_data,
+            data: image_input,
             contentType: false,
             processData: false,
             async: true,
